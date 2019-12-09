f474f0aa079d23ddf7c6141bff5f0cd48fef890d
wyl <959814898@qq.com>
2019-12-10 00:45:23 +0800

    springcloud开始学习

---
+++ springbootsecond/src/main/java/com/wyl/springbootsecond/controller/EchoController.java
@@ -0,0 +1,13 @@
+package com.wyl.springbootsecond.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EchoController {
+    @GetMapping(value = "/echo/second/{string}")
+    public String echo(@PathVariable String string) {
+        return "Hello Nacos Discovery " + string;
+    }
+}
